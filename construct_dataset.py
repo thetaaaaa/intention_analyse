8e43650d3dd11bf1c707cbc8d9083630d353ac55
theta <justice528@foxmail.com>
2022-09-21 16:08:14 +0800

    update

--- construct_dataset.py
+++ construct_dataset.py
@@ -25,13 +25,13 @@ train_corpus = corpus[cut_idx:]
 test_corpus = corpus[:cut_idx]
 # 标题数据集
 title_train, title_test = train_corpus[['label', 'title']], test_corpus[['label', 'title']]
-title_train.to_csv('xmc/title/train.tsv', sep='\t', index=0)
-title_test.to_csv('xmc/title/test.tsv', sep='\t', index=0)
+title_train.to_csv('xmc/title/train.tsv', sep='\t', index=0, header=None)
+title_test.to_csv('xmc/title/test.tsv', sep='\t', index=0, header=None)
 # 描述文本数据集
 desc_train, desc_test = train_corpus[['label', 'desc']], test_corpus[['label', 'desc']]
-desc_train.to_csv('xmc/desc/train.tsv', sep='\t', index=0)
-desc_test.to_csv('xmc/desc/test.tsv', sep='\t', index=0)
+desc_train.to_csv('xmc/desc/train.tsv', sep='\t', index=0, header=None)
+desc_test.to_csv('xmc/desc/test.tsv', sep='\t', index=0, header=None)
 # 标题——描述文本数据集
 tile_desc_train, tile_desc_test = train_corpus[['label', 'title', 'desc']], test_corpus[['label', 'title', 'desc']]
-tile_desc_train.to_csv('xmc/title_desc/train.tsv', sep='\t', index=0)
-tile_desc_test.to_csv('xmc/title_desc/test.tsv', sep='\t', index=0)
+tile_desc_train.to_csv('xmc/title_desc/train.tsv', sep='\t', index=0, header=None)
+tile_desc_test.to_csv('xmc/title_desc/test.tsv', sep='\t', index=0, header=None)
